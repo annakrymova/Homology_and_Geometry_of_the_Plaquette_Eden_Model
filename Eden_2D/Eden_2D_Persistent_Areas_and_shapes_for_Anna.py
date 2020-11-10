535709e405e97bacf8a1c705d8d880437cd28293
annakrymova <annakrymova1@gmail.com>
2020-11-10 14:33:59 +0100

    -- created betti_1_total_vector -- added reading of Eden from txt file -- added the plots

--- Eden_2D/Eden_2D_Persistent_Areas_and_shapes_for_Anna.py
+++ Eden_2D/Eden_2D_Persistent_Areas_and_shapes_for_Anna.py
@@ -4,8 +4,9 @@ import random
 from tqdm import tqdm
 from Drawing import draw_barcode, draw_polyomino, draw_polyomino_holes
 from Grow_Eden_Supplementary_Functions import start_eden_2d, actualize_neighbors, neighbours_diag, actualize_vef, \
-    increment_betti_1_euler, increment_betti_1, barcode_forest
-
+    increment_betti_1_euler, increment_betti_1, barcode_forest, num_holes
+from Plots import plot_b_per, draw_diagram_holes, draw_tri_tetra
+import os
 """
 Created on Thu Feb 13 19:04:05 2020
  Hi Anna, with this script you can:
@@ -87,7 +88,6 @@ def grow_eden_debugging(t, ordered_tiles):
     edges = 4
 
     eden, perimeter = start_eden_2d()
-    process = [(0, 0)]
 
     holes = {}
     total_holes = 0
@@ -98,9 +98,10 @@ def grow_eden_debugging(t, ordered_tiles):
     betti_1_total = 0
     betti_1_vector = []
     betti_1_euler_total = 0
-    betti_1_euler_total_vector = []
+    betti_1_total_vector = [0]
+    len_perimeter = [4]
 
-    for i in range(1, t):
+    for i in tqdm(range(1, t)):
         tile_selected = ordered_tiles[i]
         perimeter.remove(tile_selected)
         eden[tile_selected][0] = 1
@@ -116,25 +117,54 @@ def grow_eden_debugging(t, ordered_tiles):
                                                                                             created_holes, tags)
         betti_1_vector = betti_1_vector + [betti_1]
         betti_1_total = betti_1_total + betti_1
+        betti_1_total_vector += [betti_1_total]
 
         betti_1_euler_total = increment_betti_1_euler(vertices, edges, i)
+        if betti_1_total != betti_1_euler_total:
+            raise ValueError('betti_1_total does not equal betti_1 with euler')
+        len_perimeter += [len(perimeter)]
 
         final_barcode = barcode_forest(barcode, tags)
 
-    return eden, perimeter, betti_1_vector, betti_1_euler_total_vector, barcode, holes, betti_1_total, betti_1_euler_total, created_holes, tags, final_barcode
+    return eden, perimeter, betti_1_vector, betti_1_total_vector, barcode, holes, betti_1_total, betti_1_euler_total, created_holes, tags, final_barcode, len_perimeter
+
+def read_eden_txt(filename):
+    eden = []
+    for t in open(filename).read().split('), ('):
+        # print(t)
+        a, b, c = t.strip('()[]').split(',')
+        a = a.strip()
+        b = b.strip(')')
+        c = c.strip(")]\n")
+        eden.append(((int(a), int(b)), float(c)))
+    return eden
 
 
 #####################
 # with open('3000_3D_barcode_2_24.txt','w') as f:
 #     #     f.writelines( '%s %s\n' % tuple(tu) for tu in final_barcode )
 
+Eden_f = read_eden_txt("sample_time_list.txt")
+Eden = [x[0] for x in Eden_f]
+Time = len(Eden)
+Eden, Perimeter, Betti_1_vector, Betti_1_total_vector, Barcode, Holes, Betti_1_total, Betti_1_euler_total, \
+    Created_holes, Tags, Final_barcode, Len_perimeter = grow_eden_debugging(len(Eden), Eden)
 
-Time = 2000
-Eden, Perimeter, Betti_1_total_vector, Betti_1_vector_changes, Barcode, Holes, Betti_1_total, Created_holes, Process, \
-    Perimeter_len, Tags, a = grow_eden(Time)
+
+if not os.path.exists('pictures/'+str(int(Time/1000))+'k/'):
+    os.makedirs('pictures/'+str(int(Time/1000))+'k/')
+# plot_b_per(Betti_1_total_vector, Len_perimeter, Time)
+# draw_diagram_holes(Created_holes, Holes, Time)
+Tromino, Tromino_f, Tetromino, Tetromino_f = num_holes(Created_holes, Holes)
+draw_tri_tetra(Tromino, Tromino_f, Tetromino, Tetromino_f, Time)
+
+print('a')
+# Time = 5000
+# Eden, Perimeter, Betti_1_total_vector, Betti_1_vector_changes, Barcode, Holes, Betti_1_total, Created_holes, Process, \
+#     Perimeter_len, Tags, a = grow_eden(Time)
 # draw_polyomino(Eden, Time)
-print('betti_1: ', Betti_1_total)
-print('perimeter: ', Perimeter_len[-1])
+# print('betti_1: ', Betti_1_total)
+# print('perimeter: ', Perimeter_len[-1])
 # eden, perimeter, betti_1_vector, betti_1_euler_total_vector, barcode, holes, betti_1_total, betti_1_euler_total, created_holes, tags, final_barcode\
 #     = grow_eden_debugging(Time, Process)
 # print(betti_1_total, betti_1_euler_total)
